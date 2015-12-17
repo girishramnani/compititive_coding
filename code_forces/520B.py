75e6536d4cf4de9da19ebf81cd12709cd8e8e84a
girishramnani <girishramnani95@gmail.com>
2015-12-18 00:47:01 +0530

    next

--- code_forces/520B.py
+++ code_forces/520B.py
@@ -1,20 +1,24 @@
-mem =dict()
+count=0
+m,n = [int(x) for x in input().split()]
 
-def ans(x,y):
-    import pdb
-    pdb.set_trace()
-    
-    if x<1 or y <1:
-        return 0
-    if x==y:
-        return 0
-    elif x>y:
-        return an
-    
-    an =1+ min(ans(x*2,y),ans(x-1,y))
-    mem[x] = an
-    return an
-        
-t =4
-w=6
-print(ans(4,6))
+queue=[(m,0)]
+visited=set([m])
+minstep=float("inf")
+while queue:
+    num,step = queue.pop(0)
+    if num==n:
+        if step < minstep:
+            minstep = step
+    if num > n:
+        adder = num-n
+        queue.append((n,step+adder))
+
+    else:
+        for adder in [lambda x:x-1,lambda x:x*2]:
+            ne = adder(num)
+            
+            if ne==n or (ne not in visited and ne >0):
+                queue.append((ne,step+1))         
+                visited.add(ne)
+print(minstep)       
+
