a3aaaef77f3effcaca8165e6b63d945d37bc6a17
girishramnani <girishramnani95@gmail.com>
2016-04-07 20:50:53 +0530

    created clrs folder

--- prob9.py
+++
@@ -1,8 +0,0 @@
-import math
-for i in range(200,1000):
-    for j in range(200,1000):
-        for k in range(200,1000):
-            
-            if (i**2)+(j**2) == (k**2) and i+j+k == 1000:
-                print(i*j*k)
-                break
--- untitled0.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 19 20:55:28 2015
-
-@author: girish
-"""
-
